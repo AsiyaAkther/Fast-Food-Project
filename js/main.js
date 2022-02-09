9519b7c939346652f316362e331d99f014007af3
Aptech Computer <mahicse28@gmail.com>
2022-02-09 19:53:21 +0600

    added some new sections

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-new WOW().init();
+$(document).ready(function(){
+    new WOW().init();
 
-//jQuery Counter
-    $('.counter').counterUp({
-        time : 3000
-    });
-	
-	$('.quotes-slider').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
+    //jQuery Counter
+        $('.counter').counterUp({
+            time : 3000
+        });
+        
+        $('.quotes-slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
         }
-    }
-})
+    })
+
+    $('.slider').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
